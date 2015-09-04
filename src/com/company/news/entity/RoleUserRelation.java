f5a728bbdd8ef33dc73d7a7652def6b5e03007f7
yjjewg11 <41049630@qq.com>
2015-09-04 23:21:23 +0800

    修复有园长权限,不能授权bug. 增加接口,根据电话获取用户,userinfo/getUserBytel.json?tel=123123

--- src/com/company/news/entity/RoleUserRelation.java
+++ src/com/company/news/entity/RoleUserRelation.java
@@ -1,5 +1,7 @@
 package com.company.news.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -16,6 +18,14 @@ public class RoleUserRelation extends IdEntity{
 	  
 	  @Column
 	  private String groupuuid;//幼儿园id.角色关联到用户关联的幼儿园
+	  	
+	  @Column
+	  private String create_user;//品牌名称
+	  @Column
+	  private String create_useruuid;//公司全称
+	  @Column
+	  private Timestamp create_time;//创建时间
+	  
 	public String getRoleuuid() {
 		return roleuuid;
 	}
@@ -33,6 +43,24 @@ public class RoleUserRelation extends IdEntity{
 	}
 	public void setGroupuuid(String groupuuid) {
 		this.groupuuid = groupuuid;
+	}
+	public String getCreate_user() {
+		return create_user;
+	}
+	public void setCreate_user(String create_user) {
+		this.create_user = create_user;
+	}
+	public String getCreate_useruuid() {
+		return create_useruuid;
+	}
+	public void setCreate_useruuid(String create_useruuid) {
+		this.create_useruuid = create_useruuid;
+	}
+	public Timestamp getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(Timestamp create_time) {
+		this.create_time = create_time;
 	}
 	  
 
