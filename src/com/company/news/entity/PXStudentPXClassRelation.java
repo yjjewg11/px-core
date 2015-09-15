8496926a86a9e4010a6533f67ea1ed5f070c8e94
yjjewg11 <yjjewg11>
2015-09-15 18:00:36 +0800

    新增学生模块

--- src/com/company/news/entity/PXStudentPXClassRelation.java
+++ src/com/company/news/entity/PXStudentPXClassRelation.java
@@ -19,5 +19,17 @@ public class PXStudentPXClassRelation extends IdEntity {
 	private String student_uuid;// 学生uuid
 	@Column
 	private String class_uuid;// 班级uuid
+	public String getStudent_uuid() {
+		return student_uuid;
+	}
+	public void setStudent_uuid(String student_uuid) {
+		this.student_uuid = student_uuid;
+	}
+	public String getClass_uuid() {
+		return class_uuid;
+	}
+	public void setClass_uuid(String class_uuid) {
+		this.class_uuid = class_uuid;
+	}
 
 }
