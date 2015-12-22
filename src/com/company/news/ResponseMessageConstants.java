1048541b8c0747d98db5cb5553320261363b1053
yjjewg11 <41049630@qq.com>
2015-12-22 16:46:47 +0800

    增加常量ResponseMessageConstants, //请求返回消息描述.

---
+++ src/com/company/news/ResponseMessageConstants.java
@@ -0,0 +1,14 @@
+package com.company.news;
+
+public class ResponseMessageConstants {
+	  //请求返回消息描述.
+	//禁止 
+	  static public String Check_status_disable="此内容被多人举报,内容无法进行查看";
+	  
+	  static public String Data_deleted="此内容已经被删除";
+	  
+	  
+	  static public String SessionTimeout="已在其他设备登录,请重新登录.";
+	  static public String Nopower="没有权限执行该操作";
+	  static public String success_msg="操作成功";
+}
